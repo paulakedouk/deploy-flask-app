05ff6fc4b4a14d3fa22f0187c7a154372a2d8b30
Paula Kedouk <paulakedouk@gmail.com>
2020-03-09 10:32:08 -0700

    Breaking the test file

--- test_main.py
+++ test_main.py
@@ -24,7 +24,7 @@ def client():
 
 def test_health(client):
     response = client.get('/')
-    assert response.status_code == 200
+    assert response.status_code == False
     assert response.json == 'Healthy'
 
 
@@ -35,6 +35,6 @@ def test_auth(client):
                            data=json.dumps(body),
                            content_type='application/json')
 
-    assert response.status_code == 200
+    assert response.status_code == False
     token = response.json['token']
     assert token is not None
